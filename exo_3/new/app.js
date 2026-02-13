57be653778ec684d825930abf66ed595838d3a4b
ajerome <ajerome.git@gmail.com>
2026-02-13 11:48:11 +0100

    Add login.ejs, tab-bord.ejs + root system

--- exo_3/new/app.js
+++ exo_3/new/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tab_bordRouter = require('./routes/tab_bord');
+var loginRouter = require('./routes/login');
+
 
 var app = express();
 
@@ -20,7 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/tab_bord', tab_bordRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- exo_3/new/routes/tab_bord.js
+++ exo_3/new/routes/tab_bord.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var path = require("path")
 
-/* GET home page. */
+/* GET tab_bord page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('tab_bord', { title: 'Express' });
 });
+
 
 module.exports = router;
