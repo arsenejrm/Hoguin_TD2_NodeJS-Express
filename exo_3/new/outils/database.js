1115d48e90e96b978d3fec978753197e34ccf47d
arsenejrm <ajerome.git@gmail.com>
2026-02-26 23:57:10 +0100

    Set database link to local MongoDB IUT database

--- exo_3/new/outils/database.js
+++ exo_3/new/outils/database.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-// mongodb+srv://arsucompte:doudou78@wlhcamarchemtn.h11o2rx.mongodb.net/22302352_db
-// mongodb://22302352:6hd5kubi@192.168.24.1:27017/22302352_db
 const connect_db = async () => {
     try {
-        await mongoose.connect('mongodb+srv://arsucompte:doudou78@wlhcamarchemtn.h11o2rx.mongodb.net/22302352_db');
+        await mongoose.connect('mongodb://22302352:6hd5kubi@192.168.24.1:27017/22302352_db');
         console.log("Connecté à MongoDB")
     } catch (err) {
         console.log("Erreur lors de la connexion à la base de données : " + err);
